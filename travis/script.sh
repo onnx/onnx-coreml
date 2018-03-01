a192d0c0acb54332f79a77c177fa96655c1bac2b
aseemw <aseem.elec@gmail.com>
2018-02-28 18:56:10 -0800

    use --addopts flag to run unitests in travis ci

--- travis/script.sh
+++ travis/script.sh
@@ -11,8 +11,8 @@ if [ "${PYTHON_VERSION}" != "python2" ]; then
 fi
 
 #time python setup.py test
-time python -m unittest tests.transformers_test
-time python -m unittest tests.convert_test
-time python -m unittest tests.graph_test
-time python -m unittest -m tests.operators_test
-time python -m unittest -m tests.onnx_backend_models_test
+time python setup.py test --addopts tests/transformers_test.py
+time python setup.py test --addopts tests/convert_test.py
+time python setup.py test --addopts tests/graph_test.py
+time python setup.py test --addopts tests/operators_test.py
+time python setup.py test --addopts tests/onnx_backend_models_test.py
