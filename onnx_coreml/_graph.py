510c1c5e6746a64cafb2833639a2394f01f24a4c
aseemw <aseem.elec@gmail.com>
2018-03-06 12:40:05 -0800

    Replaced only TensorProto.DataType with Any in _graph.py

--- onnx_coreml/_graph.py
+++ onnx_coreml/_graph.py
@@ -14,8 +14,7 @@ class Transformer(Protocol):
         pass
 
 
-#EdgeInfo = Tuple[Text, TensorProto.DataType, TensorShapeProto]
-EdgeInfo = Tuple[Text, Any, Any]
+EdgeInfo = Tuple[Text, Any, TensorShapeProto]
 AttributeValue = Any # TODO Union[Sequence[float], Sequence[int], Sequence[Text], Sequence[TensorProto], Sequence[GraphProto]]
 
 def _input_from_onnx_input(input):  # type: (ValueInfoProto) -> EdgeInfo
