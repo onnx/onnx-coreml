4f3eef5a944c8a558255209f1032cb349fcf0967
aseemw <aseem.elec@gmail.com>
2018-03-06 14:41:30 -0800

    triaged all the failing tests into categories

--- travis/script.sh
+++ travis/script.sh
@@ -11,3 +11,8 @@ if [ "${PYTHON_VERSION}" != "python2" ]; then
 fi
 
 #time python setup.py test
+time python setup.py test --addopts tests/transformers_test.py
+time python setup.py test --addopts tests/convert_test.py
+time python setup.py test --addopts tests/graph_test.py
+time python setup.py test --addopts tests/operators_test.py
+time python setup.py test --addopts tests/onnx_backend_test.py
